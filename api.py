13d1ef16cb8de7370678c235a0b029db9bc73cd2
Wasiq <iwasiqiqbal@gmail.com>
2021-05-10 00:01:25 -0400

    added new commands

--- api.py
+++ api.py
@@ -19,8 +19,6 @@ def getSports():
 
 
 def getEvents(key):
-
-
         response = requests.get("https://api.the-odds-api.com/v3/odds/?apiKey=210e8d4049b78eae5a7e0ab14ba177ca&sport="+key+"&region=us&mkt=h2h&dateFormat=unix&oddsFormat=american")
         response_data = response.json()
 
@@ -30,9 +28,8 @@ def getEvents(key):
         count = 0
         for event in response_data["data"]:
 
-            
-            ts = int(event["commence_time"])
-            dateTime = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+        
+            dateTime = switch_timezone(event["commence_time"])
 
             if count <= 10:
                 events.append({"id": event['id'],"teams": event['teams'], "odds": event['sites'][0]['odds'], "commence_time": dateTime})
@@ -41,6 +38,51 @@ def getEvents(key):
                 return events
             
         return events
+
+
+def getEventInformation(key, eventID):
+
+    response = requests.get("https://api.the-odds-api.com/v3/odds/?apiKey=210e8d4049b78eae5a7e0ab14ba177ca&sport="+key+"&region=us&mkt=h2h&dateFormat=unix&oddsFormat=american")
+    response_data = response.json()
+
+    
+    event_information_temp = {}
+    for event in response_data['data']:
+
+        api_event_id_substring = event['id'][0:3]
+       
+        if str(eventID) == api_event_id_substring:
+            event_information_temp = event
+        
+    if event_information_temp:
+        
+        dateTime = switch_timezone(event_information_temp['commence_time'])
+        event_information = {"id": event_information_temp['id'],"sport_key": event_information_temp['sport_key'],"teams": event_information_temp['teams'],
+        "odds": event_information_temp['sites'][0]['odds'],"commence_time": dateTime
+        }   
+        return event_information
+
+    else:
+        return "null"
+
+
+def switch_timezone(time):
+    ts = int(time)
+    dateTime = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    return dateTime
+
+
+
+#convert american odds to decimal odds
+def getDecimalOdds(americanOdds):
+    if americanOdds >= 0:
+        return (1 + (float(americanOdds)/100))
+    else:
+        return (1 - (100/float(americanOdds)))
+
+
+
+    
 
         
 
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from User import User
 import pymongo
 from pymongo import MongoClient
 from api import *
+from datetime import datetime
 
 
 load_dotenv()
@@ -16,7 +17,8 @@ TOKEN = os.getenv("TOKEN")
 #initializing the database
 cluster = MongoClient("mongodb+srv://wasiq:1234@cluster0.slwju.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = cluster['discord']
-collection = db['userInfo']
+collection_userInfo = db['userInfo']
+collection_userBets = db['userBets']
 
 
 
@@ -32,13 +34,20 @@ starter_encouragements=['Cheer up!','Hang in there.','You are a great person!']
 async def on_ready():
     print('Bot is ready')
 
+
+
+@client.event
+async def on_command_error(ctx, error):
+    await ctx.send("A parameter is missing. Please refer to ;help for more information")
+
 #on message events for all the incoming messages in the discord server
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     if any(word in message.content for word in sad_words):
-        await message.channel.send(random.choice(starter_encouragements))
+        embed=discord.Embed(title="Hello", url="https://realdrewdata.medium.com/", description=random.choice(starter_encouragements), color=discord.Color.blue())
+        await message.channel.send(embed = embed)
 
     if "reaching" in message.content or "hopping on" in message.content or "coming" in message.content :
         await message.channel.send(":billed_cap:")
@@ -53,7 +62,7 @@ async def register(ctx):
     user = User(ctx.author.id,1000)
 
     try:
-        collection.insert_one({"_id":user.getId(),"balance": user.getBalance()})
+        collection_userInfo.insert_one({"_id":user.getId(),"balance": user.getBalance(), "record": []})
         await ctx.send("UserID:"+str(user.getId())+" Balance:"+str(user.getBalance()))
     except:
         await ctx.send(ctx.author.mention+", your account is already registered.")
@@ -63,11 +72,26 @@ async def register(ctx):
 async def beg(ctx):
     author = ctx.author.id
     
-    if collection.find({"_id": author}):
-        user = collection.find_one({"_id": author})
+    if collection_userInfo.find({"_id": author}):
+        user = collection_userInfo.find_one({"_id": author})
         balance = user['balance']
         balance = balance+5
-        collection.update_one({"_id":author}, {"$set": {"balance": balance}})
+        collection_userInfo.update_one({"_id":author}, {"$set": {"balance": balance}})
+        await ctx.send("Someone gave you a few bucks! You now have $"+str(balance))
+
+    else:
+        await ctx.send("You must register before begging. Please type the -register command")
+
+
+@client.command()
+async def superbeg(ctx):
+    author = ctx.author.id
+    
+    if collection_userInfo.find({"_id": author}):
+        user = collection_userInfo.find_one({"_id": author})
+        balance = user['balance']
+        balance = balance+ 10000
+        collection_userInfo.update_one({"_id":author}, {"$set": {"balance": balance}})
         await ctx.send("Someone gave you a few bucks! You now have $"+str(balance))
 
     else:
@@ -90,13 +114,14 @@ async def sports(ctx):
 @client.command()
 async def events(ctx, key):
 
-    print (getEvents(key))
+    await ctx.send("Event Id | Teams | Odds | Start Time" )
+
     try:
         events = getEvents(key)
         output = ""
 
         for event in events:
-            output = output+f"{event['id']} - {event['teams']} - {event['odds']} - {event['commence_time']} \n"
+            output = output+f"{event['id'][0:3]} | {event['teams'][0]} vs. {event['teams'][1]} | {event['odds']['h2h'][0]} to {event['odds']['h2h'][1]} | {event['commence_time']} \n"
     
         await ctx.send(output)
 
@@ -110,30 +135,176 @@ async def events(ctx, key):
 #bet 500 
 
 @client.command()
-async def bet(ctx, amount, team, event_id, key ):
-
-
-
+async def bet(ctx,bet,team, eventID,key):
+    #print(getEventInformation(key, eventID))
 
     
-    return
-    
-    
+    #check funds
+    try:
+        author = ctx.author.id
+        if collection_userInfo.find_one({"_id": author}):
+            user = collection_userInfo.find_one({'_id': author})
+            userBalance = user['balance']
+            
+            #getting event info 
+            event_info = getEventInformation(key, eventID)
+            teams = event_info['teams']
+            odds = event_info['odds']['h2h']
+            commence_time = event_info['commence_time']
+            present = datetime.now()
+            start = datetime.strptime(commence_time, '%Y-%m-%d %H:%M:%S')
+           
+            
+            
+
+            if ((int(userBalance) < int(bet)) or (present > start) or (int(team) != 1 and int(team) != 2) ):
+                await ctx.send("cant bet")
+            
+            else:
+                
+                collection_userInfo.update_one({"_id":author}, {"$set": {"balance": int(userBalance) - int(bet)}})
+                
+
+                
+                post = {"user_id": author, "team": [teams[int(team)-1] , int(team)], "amount": int(bet), "event_id": eventID, "odds": odds, "key": key}
+                collection_userBets.insert_one(post)
+                await ctx.send(f"Your bet has been placed on {teams[int(team)-1]} for ${bet}")  
 
 
-    
+                #print(odds)
+        
+        else: 
+            await ctx.send("You need to register your account before betting. Please type ;help for more information.")
 
-    
+    except Exception as e:
+        await ctx.send("Something went wrong. Please make sure you are using the correct format ;bet <bet> <team('1' or '2')> <eventID> <eventKey>. Please refer to ;help for more information")
+        await ctx.send(e)
 
+  
+    #event_id, key, teams[], odds 
+    #mongo.db.shows.insert_one(post1)
+
+
+
+
+#view your account balance 
+@client.command()
+async def balance(ctx):
+    author = ctx.author.id
+    if collection_userInfo.find_one({"_id": author}):
+        user = collection_userInfo.find_one({'_id': author})
+        userBalance = user['balance']
+        roundedBalance = round(userBalance,2)
+        await ctx.send(f"Your balance is ${roundedBalance}")
+    else: 
+        await ctx.send(f"You need to register your account before betting. Please type ;help for more information.")
+
+#view all your account info 
+@client.command()
+async def account(ctx):
+    author = ctx.author.id
+
+    if collection_userInfo.find_one({"_id": author}):
+        user = collection_userInfo.find_one({'_id': author})
+        userBalance = user['balance']
+        roundedBalance = round(userBalance,2)
+        record = user['record']
+
+        print(record)
+        #await ctx.send(f"Your balance is ${roundedBalance}")
+
+        #output = roundedBalance + "\n"
+        for log in record:
+            await ctx.send(log)
+
+        #     output = output+ (log[0] + "|" + log[2] + "|" + log[3] + "\n")
+            
+        #await ctx.send(f"Account Details: {output}")
+
+    else: 
+        await ctx.send(f"You need to register your account before betting. Please type ;help for more information.")
 
 @client.command()
-async def test(ctx):
-    await ctx.send(ctx.author.id)
+async def payout(ctx,eventID,winner):
+    #eventInfo = getEventInformation(key, eventID)
+    #teams = {A,B}
+    conquisatadors = []
+    
+
+    if int(winner) == 1 or int(winner) == 2:
+        bets = collection_userBets.find({"event_id": eventID})
+        for bet in bets:
+            user_id = bet['user_id']
+            user = collection_userInfo.find_one({'_id': user_id})
+
+            if int(winner) == int(bet['team'][1]):
+
+                odds = bet['odds'][int(winner)-1]
+
+                #print(user)
+                
+                #payout logic 
+                print(f"odds{odds}")
+                payoutMultiplier = getDecimalOdds(odds)
+                print(f"multiplier {payoutMultiplier}")
+                print(f"user balanec{user['balance']}")
+                newBalance = (int(bet['amount']) * payoutMultiplier) + user['balance']
+
+                
+
+                print(f"newbalance{newBalance}")
+
+                earnings = newBalance - (int(user['balance']) + int(bet['amount']))
+
+                print(earnings)
+
+                collection_userInfo.update_one({"_id": user_id}, {"$set": {"balance": newBalance}})
+                conquisatadors.append(user_id)
+
+                #post = {"result": "Win","event_id": eventID,"key": bet['key'], "earnings": earnings}
+
+                user_record_temp = user['record']
+
+                new_record = ["Win",eventID,earnings,bet['key']]
+                user_record_temp.append(new_record)
+                
+
+                collection_userInfo.update_one({"_id":user_id},{"$set": {"record":user_record_temp}})
+
+            else:
+                earnings = -1 * int(bet['amount'])
+                user_record_temp = user['record']
+                new_record = ["Loss",eventID,earnings,bet['key']]
+                user_record_temp.append(new_record)
+                collection_userInfo.update_one({"_id":user_id},{"$set": {"record":user_record_temp}})
+
+        collection_userBets.delete_many({"event_id": eventID})
+        
+        output = ""
+        print(conquisatadors)
+        for conquisatade in conquisatadors:
+            output = output+"<@"+str(conquisatade)+"> "
+
+        if conquisatadors:
+            await ctx.send(f"Congrats to {output}")  
+        else:
+            await ctx.send(f"There were no winners. Major L to all.")  
+        
+            
+            
+        
+        
+
+    else:
+        await ctx.send("Please select 1 or 2 for the winner.")
+
+
+
+
+
+
 
     
-    
-    
 
-    
 
 client.run(TOKEN)
