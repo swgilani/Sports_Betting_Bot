09912abcb30fe11dde88179ff7a09669473fed53
Wasiq <iwasiqiqbal@gmail.com>
2021-05-13 20:05:41 -0400

    added a comment

--- main.py
+++ main.py
@@ -378,6 +378,7 @@ async def mybets(ctx):
 
     if not output:
         await ctx.send("You currently have no active bets.")
+        #a random comment
     else:
         bet_indices = ""
         bet_amount = ""
