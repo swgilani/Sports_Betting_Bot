ba9152e53164eb333581f90fc7917e3e3801ee85
ruchi <ruchirawperera@gmail.com>
2021-06-11 17:04:37 -0400

    fixed betting with /usr/bin/bash

--- main.py
+++ main.py
@@ -260,7 +260,7 @@ async def bet(ctx,bet,team,eventID,key):
             start = event_info['commence_time']
 
             
-        if (int(userBalance) < int(bet)):
+        if (int(userBalance) < int(bet) and int(userBalance) > 0):
             await ctx.send(f"Insufficient Funds. You have ${userBalance} to spend.")
         
         elif (int(team) != 1 and int(team) != 2):
